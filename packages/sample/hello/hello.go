9156f68384b9aa2e7963acd907168847b525ff21
jonasbn <jonasbn@gmail.com>
2022-12-17 15:40:57 +0100

    No success

--- packages/sample/hello/hello.go
+++ packages/sample/hello/hello.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 )
 
 type Request struct {
@@ -20,19 +19,7 @@ func Main(args map[string]interface{}) (*Response, error) {
 	var ip string = ""
 	var userAgent string = ""
 
-	//m := args["__ow_headers"]
-
-	for _, y := range args {
-		for _, z := range y.(interface{}).([]interface{}) {
-			userAgent = z.(map[string]interface{})["user-agent"].(string)
-			ip = z.(map[string]interface{})["do-connecting-ip"].(string)
-		}
-	}
-
-	log.Printf("ip %s", ip)
-	log.Printf("user-agent %s", userAgent)
-
 	return &Response{
-		Body: fmt.Sprintf("User-agent:%s\nIP:%s\n", userAgent, ip),
+		Body: fmt.Sprintf("User-agent:%s\nIP:%s\n%s\n", userAgent, ip, args),
 	}, nil
 }
