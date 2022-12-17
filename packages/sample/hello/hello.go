9947dc484338f124e9fc9003dfff60cef14dbc00
jonasbn <jonasbn@gmail.com>
2022-12-17 12:46:55 +0100

    Attempting to parse IP and User-agent

--- packages/sample/hello/hello.go
+++ packages/sample/hello/hello.go
@@ -15,7 +15,28 @@ type Response struct {
 }
 
 func Main(args map[string]interface{}) (*Response, error) {
+
+	var ip string = ""
+	var userAgent string = ""
+
+	for k1, v1 := range args {
+		fmt.Printf("%s is type %T\n", v1, v1)
+		if k1 == "__ow_headers" {
+			for k2, v2 := range v1 {
+				fmt.Printf("%s is type %T\n", v2, v2)
+
+				if k2 == "do-connecting-ip" {
+					ip = v2
+				}
+
+				if k2 == "user-agent" {
+					userAgent = v2
+				}
+			}
+		}
+	}
+
 	return &Response{
-		Body: fmt.Sprintf("String %s Go %v Type %T", args["__ow_headers"], args["__ow_headers"], args["__ow_headers"]),
+		Body: fmt.Sprintf("User-agent: %s\nIP %s", userAgent, ip),
 	}, nil
 }
