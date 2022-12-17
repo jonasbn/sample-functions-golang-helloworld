2376eb0bc3de8a33218509d17e5fdf96db019b05
jonasbn <jonasbn@gmail.com>
2022-12-17 13:24:37 +0100

    Attempting to output some stuff to console

--- packages/sample/hello/hello.go
+++ packages/sample/hello/hello.go
@@ -19,24 +19,20 @@ func Main(args map[string]interface{}) (*Response, error) {
 	var ip string = ""
 	var userAgent string = ""
 
-	for k1, v1 := range args {
-		fmt.Printf("%s is type %T\n", v1, v1)
-		if k1 == "__ow_headers" {
-			for k2, v2 := range v1 {
-				fmt.Printf("%s is type %T\n", v2, v2)
+	//m := args["__ow_headers"]
 
-				if k2 == "do-connecting-ip" {
-					ip = v2
-				}
-
-				if k2 == "user-agent" {
-					userAgent = v2
-				}
-			}
+	for k, v := range args {
+		switch c := v.(type) {
+		case string:
+			fmt.Printf("Item %q is a string, containing %q\n", k, c)
+		case float64:
+			fmt.Printf("Looks like item %q is a number, specifically %f\n", k, c)
+		default:
+			fmt.Printf("Not sure what type item %q is, but I think it might be %T\n", k, c)
 		}
 	}
 
 	return &Response{
-		Body: fmt.Sprintf("User-agent: %s\nIP %s", userAgent, ip),
+		Body: fmt.Sprintf("User-agent: %s\nIP: %s", userAgent, ip),
 	}, nil
 }
