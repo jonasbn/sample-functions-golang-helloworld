4f020ed0ed04e20ca6cccfb3d024a6a1c8187cc9
jonasbn <jonasbn@gmail.com>
2022-12-17 13:55:23 +0100

    Outputting types to console

--- packages/sample/hello/hello.go
+++ packages/sample/hello/hello.go
@@ -19,18 +19,10 @@ func Main(args map[string]interface{}) (*Response, error) {
 	var ip string = ""
 	var userAgent string = ""
 
-	//m := args["__ow_headers"]
+	m := args["__ow_headers"]
 
-	for k, v := range args {
-		switch c := v.(type) {
-		case string:
-			fmt.Printf("Item %q is a string, containing %q\n", k, c)
-		case float64:
-			fmt.Printf("Looks like item %q is a number, specifically %f\n", k, c)
-		default:
-			fmt.Printf("Not sure what type item %q is, but I think it might be %T\n", k, c)
-		}
-	}
+	fmt.Printf("args type: %T", args)
+	fmt.Printf("__ow_headers type: %T", m)
 
 	return &Response{
 		Body: fmt.Sprintf("User-agent: %s\nIP: %s", userAgent, ip),
