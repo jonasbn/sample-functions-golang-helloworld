e49c65c621c166a6ba4873de74dff53088913ec6
jonasbn <jonasbn@gmail.com>
2022-12-17 14:33:06 +0100

    New experimental approach

--- packages/sample/hello/hello.go
+++ packages/sample/hello/hello.go
@@ -21,6 +21,13 @@ func Main(args map[string]interface{}) (*Response, error) {
 
 	m := args["__ow_headers"]
 
+	for _, y := range args {
+		for _, z := range y.(interface{}).([]interface{}) {
+			userAgent = z.(map[string]interface{})["user-agent"].(string)
+			ip = z.(map[string]interface{})["do-connecting-ip"].(string)
+		}
+	}
+
 	return &Response{
 		Body: fmt.Sprintf("User-agent:%s\nIP:%s\nargs type:%T\now_headers type: %T\n", userAgent, ip, args, m)}, nil
 }
